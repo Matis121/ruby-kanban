e6f1ed1ad8e4f5407ac280459fe7d279a431a24d
Matis121 <mateusz6246@gmail.com>
2025-04-17 00:36:47 +0200

    test: add tests for lists_controller

--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -1,18 +1,28 @@
 require "test_helper"
 
 class ListsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get lists_create_url
-    assert_response :success
+  setup do
+    @board = boards(:board1)
+    @list = lists(:list1)
+    @user = users(:user1)
+    sign_in @user
   end
 
-  test "should get update" do
-    get lists_update_url
-    assert_response :success
+  test "should create list" do
+    assert_difference("List.count") do
+      post board_lists_path(@board), params: {
+        list: {
+          title: "New test list"
+        }
+      }
+    end
+    assert_redirected_to board_path(@list.board)
   end
 
-  test "should get destroy" do
-    get lists_destroy_url
-    assert_response :success
+  test "should destory list" do
+    assert_difference("List.count", -1) do
+      delete board_list_path(@board, @list)
+    end
+    assert_redirected_to board_path(@list.board)
   end
 end
