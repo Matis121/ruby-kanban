3f1a77b42e981edb83c3db1377e345ee825bac67
Matis121 <mateusz6246@gmail.com>
2025-04-17 00:29:17 +0200

    test: add test for boards_controller

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -2,22 +2,14 @@ require "test_helper"
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @board = boards(:one)
-  end
-
-  test "should get index" do
-    get boards_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_board_url
-    assert_response :success
+    @board = boards(:board1)
+    @user = users(:user1)
+    sign_in @user
   end
 
   test "should create board" do
     assert_difference("Board.count") do
-      post boards_url, params: { board: { title: @board.title, user_id: @board.user_id } }
+      post boards_path, params: { board: { title: "test title" } }
     end
 
     assert_redirected_to board_url(Board.last)
@@ -29,12 +21,12 @@ class BoardsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
-    get edit_board_url(@board)
+    get edit_board_path(@board)
     assert_response :success
   end
 
   test "should update board" do
-    patch board_url(@board), params: { board: { title: @board.title, user_id: @board.user_id } }
+    patch board_url(@board), params: { board: { title: "new title" } }
     assert_redirected_to board_url(@board)
   end
 
